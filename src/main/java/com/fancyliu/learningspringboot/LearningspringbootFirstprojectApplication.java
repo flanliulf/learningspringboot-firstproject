4d54d387434db51e23c62245c44075943fe08784
liufan <19680460@qq.com>
2019-11-01 13:22:58 +0800

    initial commit

---
+++ src/main/java/com/fancyliu/learningspringboot/LearningspringbootFirstprojectApplication.java
@@ -0,0 +1,14 @@
+package com.fancyliu.learningspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningspringbootFirstprojectApplication {
+
+    public static void main(String[] args) {
+        System.out.println("This is my first Spring Boot project.");
+        SpringApplication.run(LearningspringbootFirstprojectApplication.class, args);
+    }
+
+}
